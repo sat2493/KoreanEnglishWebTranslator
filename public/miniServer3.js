3570db86060c50f5df011e39a8c6c846b9c97c4f
Simon Tabligan <satabligan@ucdavis.edu>
2019-04-30 01:09:40 +0000

    Installed node_modules and Added returnPalindrome function

---
+++ public/miniServer3.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const port = 52520//30057
+
+function queryHandler(req, res, next) {
+    let url = req.url;
+    let qObj = req.query;
+    console.log(qObj);
+    if (qObj.animal != undefined) {
+	res.json( {"beast" : qObj.animal} );
+    }
+    else {
+	next();
+    }
+}
+
+function returnPalindrome(req, res) {
+    let url = req.url;
+    let word = req.query.word;
+
+    for (i = word.length - 1; i >= 0; i--) {
+        word += word[i];
+    }
+
+    let object = {"palindrome" : word} 
+
+    res.type('text/plain');
+    res.send(object);
+
+    console.log(JSON.stringify(object));
+}
+
+function fileNotFound(req, res) {
+    let url = req.url;
+    res.type('text/plain');
+    res.status(404);
+    res.send('Cannot find '+url);
+    }
+
+// put together the server pipeline
+const app = express()
+app.use(express.static('public'));  // can I find a static file? 
+app.get('/query', queryHandler );   // if not, is it a valid query?
+app.use( returnPalindrome );
+app.use( fileNotFound );            // otherwise not found
+app.listen(port, function (){console.log('Listening...');} )
+ 
