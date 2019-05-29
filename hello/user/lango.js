a581784d74e3b81e137eae0c725b7f616c882dd4
Simon Tabligan <satabligan@ucdavis.edu>
2019-05-29 01:28:44 +0000

    Lango.js: Sorted functions into methods of 3 new classes

--- hello/user/lango.js
+++ hello/user/lango.js
@@ -1,57 +1,99 @@
-function Header() {
-  return React.createElement("header", {
-    id: "header"
-  }, React.createElement("h1", {
-    id: "logo"
-  }, "Lango!"), React.createElement("button", {
-    id: "review-button",
-    type: "button",
-    name: "button",
-    onClick: function() {
-      storeResponse();
-    }
-  }, "Start Review"));
-}
+// Deals with rendering the entire page
+// Contains header, and footer
+// Depending on the value of this.state, it can contain either CreateCardMain or ReviewCardMain
+class WholePage extends React.Component {
+  constructor(props) {
+    super(props);
+   
+    // set the default value of this.state to the create cards view just for now, and testing purposes
+    this.state = { view: "create" };
+    this.Header = this.Header.bind(this);
+    this.Footer = this.Footer.bind(this);
+  }
 
-function CheckReturn(event) {
-  if(event.charCode == 13) {
-    makeRequest();
+  render() {
+    // for now passes only the CreateCardMain view
+    return React.createElement("main", null, this.Header(), new CreateCardMain().render(), this.Footer());
+  }
+
+  Header() {
+    return React.createElement("header", {
+      id: "header"
+    }, React.createElement("h1", {
+      id: "logo"
+    }, "Lango!"), React.createElement("button", {
+      id: "review-button",
+      type: "button",
+      name: "button",
+      onClick: function() {
+        storeResponse();}    
+    }, "Start Review"));
+  }
+
+  Footer() {
+    return React.createElement("footer", {
+      id: "footer"
+    }, "Username");
   }
 }
 
-function StoreCard() {
-  makeRequest();
-  storeResponse();
+// Only responsible for the "cards" div and its functionality
+class CreateCardMain extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.CheckReturn = this.CheckReturn.bind(this);
+    this.StoreCard = this.StoreCard.bind(this);
+    this.InputCard = this.InputCard.bind(this);
+  }
+
+  render() {
+    return this.InputCard();
+  }
+
+  CheckReturn(event) {
+    if(event.charCode == 13) {
+      makeRequest();
+    }
+  }
+
+  StoreCard() {
+    makeRequest();
+    storeResponse();
+  }
+
+  InputCard() {
+    return React.createElement("div", {
+      className: "cards"
+    }, React.createElement("div", {
+      className: "cardside"
+    }, React.createElement("textarea", {
+      id: "english",
+      placeholder: "English",
+      onKeyPress: this.CheckReturn
+    }), React.createElement("div", {
+      id: "outputDiv"
+    }, React.createElement("p", {
+      "data-placeholder": "Korean",
+      contentEditable: true,
+      id: "outputGoesHere"
+    }))), React.createElement("button", {
+      id: "save-button",
+      type: "button",
+      name: "button",
+      onClick: this.StoreCard
+    }, "save"));
+  }
+
 }
 
-function InputCard() {
-  return React.createElement("div", {
-    className: "cards"
-  }, React.createElement("div", {
-    className: "cardside"
-  }, React.createElement("textarea", {
-    id: "english",
-    placeholder: "English",
-    onKeyPress: CheckReturn
-  }), React.createElement("div", {
-    id: "outputDiv"
-  }, React.createElement("p", {
-    "data-placeholder": "Korean",
-    contenteditable: true,
-    id: "outputGoesHere"
-  }))), React.createElement("button", {
-    id: "save-button",
-    type: "button",
-    name: "button",
-    onClick: StoreCard
-  }, "save"));
-}
+// Only responsible for the div that displays the review cards portion
+class ReviewCardMain extends React.Component {}
 
-function Footer() {
-  return React.createElement("footer", {
-    id: "footer"
-  }, "Username");
-}
+// instantiate our html contents and its data
+const WholePageInstance = new WholePage();
 
-var main = React.createElement("main", null, Header(), InputCard(), Footer());
-ReactDOM.render(main, document.getElementById('root'));
+ReactDOM.render(
+  WholePageInstance.render(),
+  document.getElementById('root')
+);
